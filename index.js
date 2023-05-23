6a5b9c39e22d755cedf66be15bef402a30cbbb11
Shaon Majumder <shaon.majumder@byslglobal.com>
2023-05-23 15:18:42 +0600

    Working

---
+++ index.js
@@ -0,0 +1,39 @@
+export const convertFormData = (keyMain,common,dataForm) => {
+    for (const [key, value] of Object.entries(common)) {
+      if(value && typeof value == 'object'){
+        if(value instanceof File){
+          if(keyMain != ''){
+            dataForm.append(`${keyMain}[${key}]`, filterValue(value));
+          }else{
+            dataForm.append(`${key}`, filterValue(value));
+          }
+        }
+        else if(value && value.getMonth !== "undefined" && typeof value.getMonth === 'function')
+        {
+          // handle date objects
+          // if(value == null)
+          // {
+          //   console.log('null in object')
+          // }
+          if(keyMain != ''){
+            dataForm.append(`${keyMain}[${key}]`, filterValue(value));
+          }else{
+            dataForm.append(`${key}`, filterValue(value));
+          }
+        }else{
+          if(keyMain != ''){
+            dataForm = convertFormData(keyMain+'['+key+']',value,dataForm)
+          }else{
+            dataForm = convertFormData(`${key}`,value,dataForm)
+          }
+        }
+      }else{
+        if(keyMain != ''){
+          dataForm.append(`${keyMain}[${key}]`, filterValue(value));
+        }else{
+          dataForm.append(`${key}`, filterValue(value));
+        }
+      }
+    }
+    return dataForm;
+  };
