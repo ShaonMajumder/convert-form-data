985624f33367efbda943031d6d0f68d12ff4e64b
Shaon Majumder <shaon.majumder@byslglobal.com>
2023-05-23 18:57:13 +0600

    package.json edit

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 export const convertFormData = (keyMain,common,dataForm) => {
-    for (const [key, value] of Object.entries(common)) {
-      if(value && typeof value == 'object'){
-        if(value instanceof File){
-          if(keyMain != ''){
-            dataForm.append(`${keyMain}[${key}]`, filterValue(value));
-          }else{
-            dataForm.append(`${key}`, filterValue(value));
-          }
-        }
-        else if(value && value.getMonth !== "undefined" && typeof value.getMonth === 'function')
-        {
-          // handle date objects
-          // if(value == null)
-          // {
-          //   console.log('null in object')
-          // }
-          if(keyMain != ''){
-            dataForm.append(`${keyMain}[${key}]`, filterValue(value));
-          }else{
-            dataForm.append(`${key}`, filterValue(value));
-          }
-        }else{
-          if(keyMain != ''){
-            dataForm = convertFormData(keyMain+'['+key+']',value,dataForm)
-          }else{
-            dataForm = convertFormData(`${key}`,value,dataForm)
-          }
-        }
-      }else{
+  for (const [key, value] of Object.entries(common)) {
+    // console.log(key,value)
+    if(value && typeof value == 'object'){
+      if(value instanceof File){
         if(keyMain != ''){
           dataForm.append(`${keyMain}[${key}]`, filterValue(value));
         }else{
           dataForm.append(`${key}`, filterValue(value));
         }
       }
+      else if(value && value.getMonth !== "undefined" && typeof value.getMonth === 'function')
+      {
+        // handle date objects
+        // if(value == null)
+        // {
+        //   console.log('null in object')
+        // }
+        if(keyMain != ''){
+          dataForm.append(`${keyMain}[${key}]`, filterValue(value));
+        }else{
+          dataForm.append(`${key}`, filterValue(value));
+        }
+      }else{
+        if(keyMain != ''){
+          dataForm = convertFormData(keyMain+'['+key+']',value,dataForm)
+        }else{
+          dataForm = convertFormData(`${key}`,value,dataForm)
+        }
+      }
+    }else{
+      if(keyMain != ''){
+        dataForm.append(`${keyMain}[${key}]`, filterValue(value));
+      }else{
+        dataForm.append(`${key}`, filterValue(value));
+      }
     }
-    return dataForm;
-  };
+  }
+  return dataForm;
+};
